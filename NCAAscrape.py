1aa7c12969fa2b961e6042d64450fb1f10045868
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-02 14:27:34 -0600

    Grabbed container of teams

--- NCAAscrape.py
+++ NCAAscrape.py
@@ -1,8 +1,21 @@
 # Trying to scrape http://stats.ncaa.org/team/inst_team_list?sport_code=MSO&division=1
 
 from requests import get
+from bs4 import BeautifulSoup
 
 url = 'http://stats.ncaa.org/team/inst_team_list?sport_code=MSO&division=1'
 
 response = get(url)
-print(response.text[:500])
+#print(response.text[:500])
+
+html_soup = BeautifulSoup(response.text, 'html.parser')
+
+team_containers = html_soup.find_all('div', class_ = 'css-panes')
+
+print(type(team_containers))
+print(len(team_containers))
+
+print(team_containers)
+
+
+
