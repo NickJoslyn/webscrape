b50b1547f43f2f22db494acec27ab1f71d0a8b54
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-02 16:06:07 -0600

    Step 1 Complete. Send to GitHub.

--- testNCAA.py
+++ testNCAA.py
@@ -1,5 +1,5 @@
 # Trying to scrape http://stats.ncaa.org/team/inst_team_list?sport_code=MSO&division=1
-
+'''
 from requests import get
 from bs4 import BeautifulSoup
 
@@ -25,3 +25,4 @@ print(team_container.td.a.text)
 team_table = team_containers.find_all('td', valign_ = 'top')
 
 print(team_table)
+'''
