e8d82ecf7e4e8a118dc8a5a3728400641930e4a1
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-02 21:18:01 -0600

    Can find opponent and result for single site

--- NCAAStep2.py
+++ NCAAStep2.py
@@ -9,5 +9,25 @@ response = get(url)
 html_soup = BeautifulSoup(response.text, 'html.parser')
 team_container = html_soup.find('div', id = 'contentarea')
 
-print(team_container.fieldset.legend.a.text)
+scheduleTable = team_container.find_all('td', width = "50%")[0]
+date = []
+opponent = []
+result = []
 
+#Still need to find date
+gameInformation = scheduleTable.find_all('a')
+counter = 0
+for game in gameInformation:
+    if (counter%2 == 0):
+        opponent.append(game.text)
+    else:
+        result.append(game.text)
+    counter+=1
+
+print(opponent)
+print("\n")
+print(result)
+
+
+#Still need to find stats
+statsTable = team_container.find_all('td', width = "50%")[1]
