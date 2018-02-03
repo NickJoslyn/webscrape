7b281c2ccfbdb502b70d8df013ff97b8046e0823
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-03 14:25:57 -0600

    Extracts schedule/result information from single website

--- NCAAStep2.py
+++ NCAAStep2.py
@@ -14,20 +14,28 @@ date = []
 opponent = []
 result = []
 
-#Still need to find date
-gameInformation = scheduleTable.find_all('a')
+matchInformation = scheduleTable.find_all('td', class_ = "smtext")
 counter = 0
-for game in gameInformation:
-    if (counter%2 == 0):
-        opponent.append(game.text)
+
+for match in matchInformation:
+    if (counter == 0):
+        date.append(match.text)
+        counter += 1
+    elif (counter == 1):
+        opponent.append(match.find('a').text)
+        counter += 1
     else:
-        result.append(game.text)
-    counter+=1
+        result.append(match.find('a').text)
+        counter = 0
 
+'''
+print("Date: " + str(len(date)))
+print(date)
+print("\n Opponent: " + str(len(opponent)))
 print(opponent)
-print("\n")
+print("\n Result: " + str(len(result)))
 print(result)
-
+'''
 
 #Still need to find stats
 statsTable = team_container.find_all('td', width = "50%")[1]
